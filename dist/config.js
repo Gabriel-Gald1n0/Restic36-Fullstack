892882cf422ba3e71afa915399eee85e58eaaed8
gabriel <galdinobrossx1@gmail.com>
2024-08-24 23:23:36 -0300

    Ignorar src/ts/config.ts , /dist e atualizar .gitignore

--- dist/config.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = '';
-export const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-export const imgApiKey = '';
-export const imgApiUrl = 'https://api.pexels.com/v1/search';
--- src/ts/config.ts
+++
@@ -1,5 +0,0 @@
-export const apiKey = '9b00b59dfa363b5c5ed24cc17a13b283';
-export const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-
-export const imgApiKey = 'n6znS2MxVBSZBFoeNrFyQ0P3oslSKIQ5L1Z26n1Wi7hAunoicGaKQBFg';
-export const imgApiUrl = 'https://api.pexels.com/v1/search';
